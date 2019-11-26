f1ab49d6cc868337e5c3f7dc13ce3b1c1dc1b3e5
Arsen Losenko <arsenlosenko@gmail.com>
2019-11-26 15:36:34 +0200

    Add Enrty model

--- app/web/models.py
+++ app/web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    """A model which will contain the entries entered by users"""
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    email = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"<{self.name} {self.age}>"
