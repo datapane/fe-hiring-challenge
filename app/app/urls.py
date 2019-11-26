2309a6b587085467d04dce7afd5b58326214884b
Arsen Losenko <arsenlosenko@gmail.com>
2019-11-26 15:54:06 +0200

    Add view and template for home page

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web.views import HomeView
+
 urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
 ]
