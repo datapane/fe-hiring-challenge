9c5f5415232e0173341273ca3d03efb1a8eaf98a
Arsen Losenko <arsenlosenko@gmail.com>
2019-11-26 19:25:45 +0200

    Add CSVUpload form using Vue and listed uploaed CSV

--- app/web/views.py
+++ app/web/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse, FileResponse
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import FormMixin
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.core.files.storage import FileSystemStorage
 
 from web.models import Entry
 from web.forms import EntryForm
@@ -27,5 +31,16 @@ class EntryFormView(ListView, FormMixin):
         return self.get(request, *args, **kwargs)
 
 
+# this is a bad solutiion, but I didn't want ot spend more time on adding csrftoken to fetch request
+@method_decorator(csrf_exempt, name='dispatch')
 class CSVUploaderView(TemplateView):
     template_name = "csv_upload.html"
+
+    def post(self, request, *args, **kwargs):
+        if request.FILES.get('file'):
+            uploaded_file = request.FILES.get('file')
+            fs = FileSystemStorage()
+            filename = fs.save(uploaded_file.name, uploaded_file)
+            return FileResponse(open(filename, 'rb'))
+        return HttpResponse("no file included in the request")
+
