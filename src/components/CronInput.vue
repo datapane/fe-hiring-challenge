050184ae91a7ed8edff10a338ac1cf2dd18ca528
mattkatt <m4ttk4tt@hotmail.co.uk>
2023-08-19 13:34:28 +0100

    added validation to CronInput

--- src/components/CronInput.vue
+++ src/components/CronInput.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
-    <h1 class="header">Cron Input</h1>
-    <p class="mt-4">{{foo}}</p>
+  <div class="container">
+    <input v-model="cron" ref="inputRef" class="cron-input" :class="{invalid: !isValid}" placeholder="* * * * *">
   </div>
 </template>
 
 <script setup lang="ts">
-import {CronInputProps} from "../interfaces/components/CronInputProps"
+import { ref, watch } from "vue";
+import { CronInputProps } from "../interfaces/components/CronInputProps";
+import { isValidCron } from "../utils/isValidCron";
 
 defineProps<CronInputProps>();
+
+const cron = ref<string>('');
+const isValid = ref(true);
+
+watch(cron, () => {
+  isValid.value = cron.value === '' || isValidCron(cron.value);
+});
 
 </script>
 
 <style scoped lang="postcss">
-.header {
-  @apply font-bold text-blue-600
+.container {
+  @apply flex flex-col
+}
+
+.cron-input {
+  @apply border rounded px-2 py-1 focus:outline-none focus:border-blue-500
+}
+
+.cron-input.invalid {
+  @apply border-red-600 focus:border-red-600
 }
 </style>
