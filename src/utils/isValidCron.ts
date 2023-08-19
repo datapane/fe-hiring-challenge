c98221d2549ad45ade515fe9c9cf82282d55a276
mattkatt <m4ttk4tt@hotmail.co.uk>
2023-08-19 12:03:00 +0100

    initialise testing for isValidCron

---
+++ src/utils/isValidCron.ts
@@ -0,0 +1,6 @@
+const VALID_DAYS = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const VALID_MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+export function isValidCron(cron: string) {
+    return false;
+}
